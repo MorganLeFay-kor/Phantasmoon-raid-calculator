b13a6a7b72e1c8fa16c62eba0bac7ab046bbe1a8
MorganLeFay-kor <mybunshin@gmail.com>
2026-07-28 18:27:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,29 @@ function calculateRaid1() {
 
     document.getElementById("totalAP1").textContent = totalAP;
 }
+
+function calculateRaid2() {
+
+    const maxAP = Number(document.getElementById("ap2").value);
+    const gold = Number(document.getElementById("gold2").value);
+    const silver = Number(document.getElementById("silver2").value);
+    const blue = Number(document.getElementById("blue2").value);
+    const bronze = Number(document.getElementById("bronze2").value);
+
+    const totalAP =
+        (gold * maxAP) +
+        (silver * Math.ceil(maxAP / 2)) +
+        (blue * 40) +
+        (bronze * 10);
+
+    document.getElementById("totalAP2").textContent = totalAP;
+}
+
+function calculateAll() {
+    calculateRaid1();
+    calculateRaid2();
+}
+
+document.addEventListener("input", calculateAll);
+
+window.onload = calculateAll;
